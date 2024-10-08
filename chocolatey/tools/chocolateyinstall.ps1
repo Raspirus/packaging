938b9ca2c38765beb1cdbb7852cd17b837af704e
Benjamin Demetz <50681275+Benji377@users.noreply.github.com>
2024-10-08 23:34:37 +0200

    Arch package ready

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'raspirus_1.2.0_x64_en-US.msi'
+$fileLocation = Join-Path $toolsDir 'raspirus_2.0.0_x64_en-US.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  file         = $fileLocation
+  file          = $fileLocation
   softwareName  = 'raspirus'
-  checksum      = '23c9caa80d6657822e7bb004a59af8fdf4205210f33c7dccdfbda96c1f12623d'
+  checksum      = 'bf37bf8b199c8ad4765f792506dcf496091f3ccf8e0ca1154e9223ae23241ec1'
   checksumType  = 'sha256'
-  checksum64    = '23c9caa80d6657822e7bb004a59af8fdf4205210f33c7dccdfbda96c1f12623d'
+  checksum64    = 'bf37bf8b199c8ad4765f792506dcf496091f3ccf8e0ca1154e9223ae23241ec1'
   checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
